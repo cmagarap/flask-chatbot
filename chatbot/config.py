c68677761fb725ebfe4641954880347e4b7bee67
cmagarap <chrisjohn.seej@gmail.com>
2020-11-28 20:37:04 +0800

    modified config, edited gitignore, retrained the model, and changed the password of the user

--- chatbot/config.py
+++ chatbot/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('CHATBOT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
